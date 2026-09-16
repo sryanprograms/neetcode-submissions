238d5ca6cf080c9f777b1d321799be1b75b6b5f1
sryanprograms <98987115+sryanprograms@users.noreply.github.com>
2026-09-16 14:51:20 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable=True
+print(type(variable))
+variable = "hey"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
